823f52d52624bbf5900b555ebc68c5ecc0a93365
rafsalas <rafsalas@tamu.edu>
2015-10-04 16:22:47 -0500

    Adding Matlab analysis code

---
+++ Matlab Analysis/main.c
@@ -0,0 +1,61 @@
+#include "inc/hw_ints.h"
+#include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/debug.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/adc.h"
+#include "driverlib/rom.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/timer.h"
+#include "driverlib/uart.h"
+#include "driverlib/debug.h"
+#include "driverlib/fpu.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
+#include "utils/uartstdio.c"
+
+
+/*
+ * main.c
+ */
+
+#ifdef DEBUG
+void__error__(char *pcFilename, unsigned long ulLine)
+{
+}
+#endif
+int main(void) {
+	SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+
+
+
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+
+    GPIOPinTypeUART(GPIO_PORTA_BASE,  GPIO_PIN_0 | GPIO_PIN_1);
+
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+    UARTStdioConfig(0, 115200, 16000000);
+	char msg[24];
+	UARTprintf("enter\n");
+    while(1){
+    	if(UARTCharsAvail(UART0_BASE)){
+    		UARTgets(msg,24);
+    		UARTprintf(msg);
+    	}
+    }
+
+
+
+	
+	return 0;
+}
