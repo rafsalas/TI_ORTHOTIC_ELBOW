aeef0fac027533f9dc2757f18ca95cf0441d2e83
Nathaniel Glaser <cmdgnathan@tamu.edu>
2015-10-10 15:52:31 -0500

    10/10 Matlab Code Update

--- UART Test Matlab/Launchpad_Test.m
+++ UART Test Matlab/Launchpad_Test.m
@@ -1,17 +1,17 @@
 %Assign s to Serial Port
-s = serial('COM8');
+s = serial('COM9');
 
 fclose(s);
 delete(s);
 clear s
 
-s=serial('COM8');
+s=serial('COM9');
 
 %Specify Settings
 %Verify with Device Manager
-set(s, 'InputBufferSize', 24);
+set(s, 'InputBufferSize', 244);
 set(s, 'FlowControl', 'none');
-set(s,'BaudRate', 115200);
+set(s, 'BaudRate', 115200);
 set(s, 'Parity', 'none');
 set(s, 'DataBits', 8);
 set(s, 'StopBit', 1);
@@ -19,9 +19,11 @@ set(s, 'StopBit', 1);
 %Open Serial Port
 fopen(s);
 
+%Prompt Data Stream
+%fprintf(s,'%c','A');
 
-%fprintf(s,'*IDN?')
-out = fscanf(s)
+out_uni = fscanf(s)
+out_byte = unicode2native(out_uni)%,'Hex') 
 
 fclose(s)
 delete(s)
